a1d6cd2860a8b9d646e29f33219f7ce30d3975a5
Derek Greene <dg74155@gmail.com>
2017-08-01 17:03:58 -0400

    new commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.Port || 3000;
 
 app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
